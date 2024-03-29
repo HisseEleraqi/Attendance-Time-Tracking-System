3aba325c09520a6c70efc5eb6c62ed8e3532f6e6
Hussien <se7s_eleraqi@hotmail.com>
2024-03-29 16:34:30 +0200

    addingModels

---
+++ AttendenceSystem/AttendenceSystem/Models/Permision.cs
@@ -0,0 +1,19 @@
+﻿namespace AttendenceSystem.Models
+{
+    public class Permision
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }         
+        public DateOnly Date { get; set; }
+
+        public bool IsApproved { get; set; }
+
+        public string Reason { get; set; }
+
+        public string Status { get; set; }
+
+ 
+        // Foreign Key
+        public Student StudentId { get; set; }
+    }
+}
---
+++ AttendenceSystem/AttendenceSystem/Models/Schedule.cs
@@ -0,0 +1,14 @@
+﻿namespace AttendenceSystem.Models
+{
+    public class Schedule
+    {
+        public int Id {  get; set; }
+        public DateOnly Date {  get; set; }
+
+        public DateOnly StartDate {  get; set; }
+        public DateOnly EndDate {  get; set; }
+
+        public TimeOnly Time {  get; set; }
+        public virtual ICollection<Track> Tracks { get; set; }
+    }
+}
