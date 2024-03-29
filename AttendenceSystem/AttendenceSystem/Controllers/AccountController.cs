98ad61edaa89698e6bdb1d64a4f3feddac9125ab
donyamohamed <donyaelhol56@gmail.com>
2024-03-29 23:01:21 +0200

    login

---
+++ AttendenceSystem/AttendenceSystem/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AttendenceSystem.Controllers
+{
+	public class AccountController : Controller
+	{
+		public IActionResult Login()
+		{
+			return View();
+		}
+	}
+}
