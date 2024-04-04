8ae52b76e23d3682a0c451c5facdb97c3b99e39f
Maryam <maryam.mohamed.elmasry112@gmail.com>
2024-04-04 20:27:05 +0200

    Updated Schedule Model

--- AttendenceSystem/AttendenceSystem/Models/Schedule.cs
+++ AttendenceSystem/AttendenceSystem/Models/Schedule.cs
@@ -5,11 +5,12 @@
         public int Id {  get; set; }
         public DateOnly Date {  get; set; }
 
-        public DateOnly StartDate {  get; set; }
-        public DateOnly EndDate {  get; set; }
+        public TimeOnly StartTime {  get; set; }
+        public TimeOnly EndTime {  get; set; }
 
-        public TimeOnly Time {  get; set; }
+       /* public TimeOnly Time {  get; set; }*/
         public int TrackId {  get; set; }
         public virtual Track Tracks { get; set; }
+
     }
 }
