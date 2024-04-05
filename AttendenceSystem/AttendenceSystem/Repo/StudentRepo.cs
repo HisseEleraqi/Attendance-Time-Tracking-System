916ffdf6f71b52ece2863b06775312a4e31148dc
Hussien <se7s_eleraqi@hotmail.com>
2024-04-05 04:58:08 +0200

    StudentRepo

---
+++ AttendenceSystem/AttendenceSystem/Repo/StudentRepo.cs
@@ -0,0 +1,29 @@
+﻿using AttendenceSystem.Data;
+using AttendenceSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AttendenceSystem.Repo
+{
+    public class StudentRepo
+    {
+        private readonly DataContext context = new DataContext();
+       
+
+        public int StudentDetails(int id)
+        {
+
+            return (context.Students.FirstOrDefault(s => s.Id == id).Id);
+
+        }
+
+        public void StudentScdule(int id)
+        {
+           
+            var StudentSchedule =context.Students.Include(d=>d.Track.Schedules).FirstOrDefault(s => s.Id == id);
+
+        }
+
+
+
+    }
+}
