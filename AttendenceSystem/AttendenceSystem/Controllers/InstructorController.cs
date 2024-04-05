c92c2507b26ff597b3408861d44d7e1070b69e04
Maryam <maryam.mohamed.elmasry112@gmail.com>
2024-04-05 13:38:40 +0200

    Added instructor controller

---
+++ AttendenceSystem/AttendenceSystem/Controllers/InstructorController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AttendenceSystem.Data;
+using System.Linq;
+
+namespace AttendenceSystem.Controllers
+{
+    public class InstructorController : Controller
+    {
+        private readonly DataContext _db=new DataContext();
+
+      
+        public IActionResult Profile()
+        {
+            int id = 2; // Get the id from the session
+            if (id == null)
+                return BadRequest();
+            var model = _db.Instructors.FirstOrDefault(a => a.Id == id);
+            if (model == null)
+                return NotFound();
+            return View(model);
+        }
+    }
+}
